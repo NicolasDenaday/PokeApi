b0f417197e0cca3585f46797960c4137f0b06972
NicolasDenaday <nicodenaday@gmail.com>
2023-05-16 20:08:23 -0300

    agrego estilos

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import "./Pokemon.css"
 
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState({});
-  let{name} = useParams()
+  let { name } = useParams()
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .then((res) => {
@@ -19,12 +20,14 @@ const Pokemon = () => {
   }
 
   return (
-    <div>
-      {pokemon.name}
-      #{pokemon.id}
-      <img src={pokemon.sprites.front_default} />
-      <img src={pokemon.sprites.front_shiny} />
-    </div>
+    <main className='pokemonInfoCard'>
+      <div>
+        <h2>{pokemon.name}</h2>
+        <small>#{pokemon.id}</small>
+        <img src={pokemon.sprites.front_default} />
+        <img src={pokemon.sprites.front_shiny} />
+      </div>
+    </main>
   );
 }
 
